00f44f64f1f25d27f2efb418a57b8c2425adf4c3
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-13 21:06:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index.map{|d, idx| puts (idx + 1).to_s + ". #{d}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.map!{|e| puts e.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any?{|c| c.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese = ""
+  i = 0
+      while i < ingredients.length
+      if cheese_types.include?(ingredients[i])
+        return ingredients[i]
+      else 
+        i = i + 1
+      end
+  end
 end
